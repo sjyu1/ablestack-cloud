88630cb64db53d9b01b94a559ef147236bea49f8
Dajeong-Park <ekwjd7182@gmail.com>
2024-04-30 13:06:30 +0900

    Update ConfigurationValue.vue

--- ui/src/views/setting/ConfigurationValue.vue
+++ ui/src/views/setting/ConfigurationValue.vue
@@ -352,9 +352,6 @@ export default {
           return []
         }
       }
-      // if (configrecord.name === 'alert.smtp.password') {
-      //   return ''
-      // }
       if (configrecord.value) {
         return String(configrecord.value)
       }
