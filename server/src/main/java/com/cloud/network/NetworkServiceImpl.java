ec36cd668f38d8eec9d50f10957f75ba5a234d89
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2024-09-09 12:30:37 +0900

    Update NetworkServiceImpl.java

--- server/src/main/java/com/cloud/network/NetworkServiceImpl.java
+++ server/src/main/java/com/cloud/network/NetworkServiceImpl.java
@@ -2949,7 +2949,7 @@ public class NetworkServiceImpl extends ManagerBase implements NetworkService, C
 
         if (name != null) {
             // name parameter length check
-            if (!NetUtils.verifyDomainNameLabel(name, true))) {
+            if (!NetUtils.verifyDomainNameLabel(name, true)) {
                 throw new InvalidParameterValueException("이름이 잘못되었습니다. 이름에는 ASCII 문자 'a'~'z', 숫자 '0'~'9', 하이픈('-')이 포함될 수 있으며 하이픈('-')으로 시작하거나 끝날 수 없으며 숫자로 시작할 수도 없습니다.");
             }
 
