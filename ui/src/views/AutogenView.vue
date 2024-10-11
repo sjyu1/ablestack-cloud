fabea9d8daffed6105a026135c4ecf2b35a39548
sjyu1 <93505580+sjyu1@users.noreply.github.com>
2024-10-11 10:17:34 +0900

    Update AutogenView.vue

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -1356,7 +1356,7 @@ export default {
             this.items = this.items.filter((row) => row.name !== 'baremetal-system-account')
           }
           if (this.apiName === 'listNetworkOfferings') {
-            this.items = this.items.filter((row) => row.name.includes('기본 L2 VLAN 네트워크오퍼링'))
+            this.items = this.items.filter((row) => row.guestiptype.includes('L2'))
             this.items = this.items.filter((row) => !row.name.includes('with ConfigDrive'))
           }
         }
