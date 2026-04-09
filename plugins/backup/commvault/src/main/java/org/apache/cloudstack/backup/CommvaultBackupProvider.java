47654fbabe74d4e782125cbd878e92088a5d9822
Dajeong-Park <ekwjd7182@gmail.com>
2026-04-09 11:14:57 +0900

    Update CommvaultBackupProvider.java

--- plugins/backup/commvault/src/main/java/org/apache/cloudstack/backup/CommvaultBackupProvider.java
+++ plugins/backup/commvault/src/main/java/org/apache/cloudstack/backup/CommvaultBackupProvider.java
@@ -159,8 +159,8 @@ public class CommvaultBackupProvider extends AdapterBase implements BackupProvid
 
     private ConfigKey<Integer> CommvaultBackupRestoreTimeout = new ConfigKey<>("Advanced", Integer.class,
             "commvault.backup.restore.timeout",
-            "30",
-            "Timeout in seconds after which qemu-img execute when restoring",
+            "1800",
+            "Timeout in seconds after which Commvault backup restore operations fail.",
             true,
             BackupFrameworkEnabled.key());
 
