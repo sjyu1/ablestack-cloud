9a9a378ad59c4de0f65da20d3c93581a18924772
Dajeong-Park <ekwjd7182@gmail.com>
2026-06-09 09:11:53 +0900

    백업 오퍼링 제거 적용

--- ui/src/config/section/compute.js
+++ ui/src/config/section/compute.js
@@ -305,7 +305,7 @@ export default {
           api: 'removeVirtualMachineFromBackupOffering',
           icon: 'scissor-outlined',
           label: 'label.backup.offering.remove',
-          message: 'label.backup.offering.remove',
+          message: 'message.backup.offering.remove',
           docHelp: 'adminguide/virtual_machines.html#restoring-vm-backups',
           dataView: true,
           args: ['virtualmachineid', 'forced'],
