99c38585ca9e190a56dcf17bac8aa810b29b0dbb
Dajeong-Park <ekwjd7182@gmail.com>
2026-06-24 09:18:34 +0900

    Update BackupManagerImpl.java

--- server/src/main/java/org/apache/cloudstack/backup/BackupManagerImpl.java
+++ server/src/main/java/org/apache/cloudstack/backup/BackupManagerImpl.java
@@ -25,6 +25,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
