4faf49dbee6730d8a25c69ea5bd89643ea5e787f
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2026-07-28 09:24:47 +0900

    SharedMountPoint flatten bandwidth 설정 추가

--- server/src/main/java/com/cloud/vm/UserVmManagerImpl.java
+++ server/src/main/java/com/cloud/vm/UserVmManagerImpl.java
@@ -658,6 +658,7 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Vir
     private int _expungeDelay;
     private int _flattenInterval;
     private int _flattenProgressInterval;
+    private int _flattenSharedMountPointBandwidth;
     private boolean _dailyOrHourly = false;
     private int capacityReleaseInterval;
     private ExecutorService _vmIpFetchThreadExecutor;
@@ -2494,9 +2495,10 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Vir
 
         _executor = Executors.newScheduledThreadPool(wrks, new NamedThreadFactory("UserVm-Scavenger"));
 
-        int fwrks = 1;
-        _flattenInterval = NumbersUtil.parseInt(configs.get("flatten.interval"), 300);
-        _flattenProgressInterval = NumbersUtil.parseInt(configs.get("flatten.progress.interval"), 15);
+        int fwrks = NumbersUtil.parseInt(configs.get(FlattenWorkers.key()), Integer.parseInt(FlattenWorkers.defaultValue()));
+        _flattenInterval = NumbersUtil.parseInt(configs.get(FlattenInterval.key()), Integer.parseInt(FlattenInterval.defaultValue()));
+        _flattenProgressInterval = NumbersUtil.parseInt(configs.get(FlattenProgressInterval.key()), Integer.parseInt(FlattenProgressInterval.defaultValue()));
+        _flattenSharedMountPointBandwidth = NumbersUtil.parseInt(configs.get(FlattenSharedMountPointBandwidth.key()), Integer.parseInt(FlattenSharedMountPointBandwidth.defaultValue()));
 
         _flattenExecutor = Executors.newScheduledThreadPool(fwrks, new NamedThreadFactory("FlattenCloneImage-Scavenger"));
         _flattenProgressExecutor = Executors.newScheduledThreadPool(1, new NamedThreadFactory("FlattenCloneProgress-Scavenger"));
@@ -9073,7 +9075,8 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Vir
         return new ConfigKey<?>[] {EnableDynamicallyScaleVm, AllowDiskOfferingChangeDuringScaleVm, AllowUserExpungeRecoverVm, VmIpFetchWaitInterval, VmIpFetchTrialMax,
                 VmIpFetchThreadPoolMax, VmIpFetchTaskWorkers, AllowDeployVmIfGivenHostFails, EnableAdditionalVmConfig, DisplayVMOVFProperties,
                 KvmAdditionalConfigAllowList, XenServerAdditionalConfigAllowList, VmwareAdditionalConfigAllowList, DestroyRootVolumeOnVmDestruction,
-                EnforceStrictResourceLimitHostTagCheck, StrictHostTags, AllowUserForceStopVm, EnableVmNetwokFilterAllowAllTraffic};
+                EnforceStrictResourceLimitHostTagCheck, StrictHostTags, AllowUserForceStopVm, EnableVmNetwokFilterAllowAllTraffic,
+                FlattenInterval, FlattenProgressInterval, FlattenWorkers, FlattenSharedMountPointBandwidth};
     }
 
     @Override
@@ -10543,6 +10546,7 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Vir
         FlattenSharedMountPointCommand flattenCommand = new FlattenSharedMountPointCommand(volumeTO);
         Map<String, String> options = new HashMap<>();
         options.put("operation", operation);
+        options.put("bandwidth", String.valueOf(_flattenSharedMountPointBandwidth));
         if (StringUtils.isNotBlank(backingPath)) {
             options.put("backingPath", backingPath);
         }
